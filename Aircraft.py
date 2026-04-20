5873e5aa1f6a97184af7dc30888d1717efb07c79
polmolinas-design <pol.molinas@estudiantat.upc.edu>
2026-04-20 17:31:43 +0200

    Update Aircraft.py

--- Aircraft.py
+++ Aircraft.py
@@ -27,29 +27,33 @@ def load_arrivals(filename):
     f.close()
     return lista_arrivals
 
-    def f_plot_arrivals(self):
-        if not self.lista_aeropuertos:
-            messagebox.showwarning("Error", "No hay datos cargados. Usa el botón 1 primero.")
-            return
+def f_plot_arrivals(self):
+        
+    if not self.lista_aeropuertos:
+        messagebox.showwarning("Error", "No hay datos cargados. Usa el botón 1 primero.")
+        return
 
-        try:
+    try:
 
-            horas_formato = [int(obj.time[:2]) for obj in self.lista_aeropuertos]
+        horas_formato = [int(obj.time[:2]) for obj in self.lista_aeropuertos]
 
-            cont_hora = [0] * 24
-            for hora in horas_formato:
-                if 0 <= hora < 24:
-                    cont_hora[hora] += 1
+        cont_hora = [0] * 24
+        for hora in horas_formato:
+            if 0 <= hora < 24:
+                cont_hora[hora] += 1
 
-            eje_x = list(range(24))
-            eje_y = cont_hora
+        eje_x = list(range(24))
+        eje_y = cont_hora
 
-            plt.figure(figsize=(10, 5))
-            plt.bar(eje_x, eje_y, color='blue', edgecolor='black')
-            plt.title("Vuelos por Hora de Llegada")
-            plt.xlabel("Hora del día")
-            plt.ylabel("Cantidad de aviones")
-            plt.xticks(eje_x)  # Para que salgan todos los números del 0 al 23
-            plt.grid(axis='y', linestyle='--', alpha=0.4)
+        plt.figure(figsize=(10, 5))
+        plt.bar(eje_x, eje_y, color='blue', edgecolor='black')
+        plt.title("Vuelos por Hora de Llegada")
+        plt.xlabel("Hora del día")
+        plt.ylabel("Cantidad de aviones")
+        plt.xticks(eje_x)  # Para que salgan todos los números del 0 al 23
+        plt.grid(axis='y', linestyle='--', alpha=0.4)
 
-            plt.show()
+        plt.show()
+
+    except AttributeError:
+        messagebox.showerror("Error", "Los objetos cargados no tienen el atributo '.time'")
