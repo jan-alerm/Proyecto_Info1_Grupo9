4055a3ad3251d0a1ba5cb95540bb709c232f3ac0
jan-alerm <jan.alerm@estudiantat.upc.edu>
2026-04-20 17:22:29 +0200

    Update airport.py

--- airport.py
+++ airport.py
@@ -162,4 +162,23 @@ class Aircraft:
         self.time = time
 
 def load_arrivals(filename):
-    f = open(filename,"r")
+    f = open(filename, 'r')
+    lista_arrivals = []
+    f.readline()
+    linea = f.readline()
+
+    while linea != "":
+        elementos = linea.split()
+
+        if len(elementos) == 4:
+            codigo = elementos[0]
+            origen = elementos[1]
+            tiempo = elementos[2]
+            company = elementos[3]
+            nueva_llegada = Aircraft(codigo, company, origen, tiempo)
+            lista_arrivals.append(nueva_llegada)
+            linea = f.readline()
+
+    f.close()
+    return lista_arrivals
+
