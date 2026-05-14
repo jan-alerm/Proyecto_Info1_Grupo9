4543449a1601209edf5646ac63c1721e5a6f2f20
jan-alerm <jan.alerm@estudiantat.upc.edu>
2026-05-15 00:31:56 +0200

    Update LEBL.py

--- LEBL.py
+++ LEBL.py
@@ -41,3 +41,33 @@ def SetGates(area, init_gate, end_gate, prefix):
         area.gates.append(new_gate)
 
     return True
+
+def LoadAirlines(terminal, t_name):
+    # Si el nombre de la terminal no concuerda con el de los ficheros:
+    if t_name == "T1" or t_name == "T2":
+        terminal_name = t_name+"_Airlines.txt"
+    else:
+        print("No terminal with that name")
+        return False
+
+    #Si no se encuentra el fichero de esa terminal:
+    try:
+        f = open(terminal_name, "r")
+    except FileNotFoundError:
+        print("The file is not found")
+        return False
+
+
+    line = f.readline() # Lee la primera línea
+    nueva_lista = [] # Crea una nueva lista vacía para añadirle las nuevas aerolíneas
+
+    while line != "":
+        codigo = line[-3:] # Solo nos interesa el ICAO code de la aerolínea que son los últimos tres carácteres
+        nueva_lista.append(codigo)
+        line = f.readline()
+
+    f.close()
+    terminal.airline_icao_codes = nueva_lista # Actualizamos la nueva lista en la terminal
+    return True
+
+
