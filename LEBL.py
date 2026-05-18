e70a8e140c168d7dc9687726c80f51a4dc8cea42
paulasegade-dot <paula.segade@estudiantat.upc.edu>
2026-05-18 13:16:18 +0200

    Update LEBL.py

--- LEBL.py
+++ LEBL.py
@@ -1,3 +1,5 @@
+import os
+
 class Gate:
     def __init__(self, name):
         self.name = name
@@ -8,7 +10,7 @@ class Gate:
 class BoardingArea:
     def __init__(self, name, area_type):
         self.name = name
-        self.type = area_type  # 'Schengen' or 'non-Schengen'
+        self.type = area_type                                                           # 'schengen' or 'non-schengen'
         self.gates = []
 
 
@@ -25,7 +27,7 @@ class BarcelonaAP:
         self.terminals = []
 
 
-def SetGates(area, init_gate, end_gate, prefix):
+def set_gates(area, init_gate, end_gate, prefix):
     """
     Updates the list of gates of a boarding area.
     Returns -1 if end_gate is not greater than init_gate.
@@ -42,12 +44,15 @@ def SetGates(area, init_gate, end_gate, prefix):
 
     return True
 
-def LoadAirlines(terminal, t_name):
-    # Si el nombre de la terminal no concuerda con el de los ficheros:
+#=========================================== CARGAR AEROLÍNEAS POR TERMINAL ============================================
+def load_airlines(terminal, t_name):
+#FUNCIÓN: carga las aerolíneas que tocan a una terminal desde los ficheros T1_airlines.txt o T2_airlines.txt
+#         si el fichero no existe, se devuelve false
+
     if t_name == "T1" or t_name == "T2":
-        terminal_name = t_name+"_Airlines.txt"
+        terminal_name = t_name + "_Airlines.txt"
     else:
-        print("No terminal with that name")
+        print("No hay terminal con ese nombre")
         return False
 
     #Si no se encuentra el fichero de esa terminal:
@@ -57,17 +62,118 @@ def LoadAirlines(terminal, t_name):
         print("The file is not found")
         return False
 
-
-    line = f.readline() # Lee la primera línea
-    nueva_lista = [] # Crea una nueva lista vacía para añadirle las nuevas aerolíneas
+    nueva_lista = []                                                           #lee la primera línea
+    line = f.readline()                                                        #nueva lista vacía para añadir aerolineas
 
     while line != "":
-        codigo = line[-3:] # Solo nos interesa el ICAO code de la aerolínea que son los últimos tres carácteres
+        codigo = line[-3:]                            #solo interesa el ICAO code de la aerolínea (últimos 3 carácteres)
         nueva_lista.append(codigo)
         line = f.readline()
 
     f.close()
-    terminal.airline_icao_codes = nueva_lista # Actualizamos la nueva lista en la terminal
+    terminal.airline_icao_codes = nueva_lista                               #actualizamos la nueva lista en la terminal
     return True
 
+#=========================================== CARGAR "ESTRUCTURA DEL AEROPUERTO" ========================================
+def load_airport_structure(filename):
+#FUNCIÓN: crea un "objeto" BarcelonaAP utilizando la información guardada en el fichero LEBL.txt
+#         la función transforma los datos del fichero para construir la estructura que se entiende:
+#          - terminales
+#          - boarding areas
+#          - gates
+#         además de cargar las aerolíneas correspondientes a cada terminal
+#         importante que la estructura del documento "terminals": AREA LETTER SCHENGEN/NON SCHENGEN GATES NUM_GATES
 
+    try:
+        f = open(filename, "r")
+    except FileNotFoundError:
+        print("The file is not found")
+        return False
+
+    # LEBL tiene 2 terminals
+    first_line = f.readline().split()
+    airport_code = first_line[0]
+    bcn = BarcelonaAP(airport_code)
+    line = f.readline()
+
+    while line != "":
+        elementos = line.split()
+
+#       -------------TERMINAL--------------
+        if elementos[0] == "Terminal":
+            terminal_name = elementos[1]
+            nueva_terminal = Terminal(terminal_name)
+#           cargar las aerolíneas de cada terminal
+            load_airlines(nueva_terminal, terminal_name)
+            num_areas = int(elementos[2])
+            i = 0
+
+            while i < num_areas:
+                area_line = f.readline().split()
+                #transformar distribución del documento
+                area_name = area_line[1]
+                area_type = area_line[2]
+                init_gate = int(area_line[4])
+                end_gate = int(area_line[6])
+                nueva_area = BoardingArea(area_name, area_type)
+
+                #definir el prefijo para ver si schengen / non schengen
+                prefix = terminal_name + area_name + "G"
+                set_gates(nueva_area, init_gate, end_gate, prefix)
+                nueva_terminal.boarding_areas.append(nueva_area)
+
+                i = i + 1
+
+            bcn.terminals.append(nueva_terminal)
+        line = f.readline()
+    f.close()
+    return bcn
+
+#=========================================== OCUPACIÓN DE LAS "GATES" TERMINALES =======================================
+def gate_occupancy(bcn):
+#FUNCIÓN: devuelve una lista del estado de ocupación de todas las puertas ("gates") del aeropuerto (T1 y T2)
+
+    lista_ocupacion = []
+    i = 0
+
+    while i < len(bcn.terminals):
+        terminal = bcn.terminals[i]
+        j = 0
+        while j < len(terminal.boarding_areas):
+            area = terminal.boarding_areas[j]
+            a = 0
+            while a < len(area.gates):
+                gate = area.gates[a]
+
+                if gate.occupied:
+                    estado = "Ocupado"
+                    aircraft = gate.aircraft_id
+                else:
+                    estado = "Free"
+                    aircraft = "-"
+
+                info_gate = [gate.name, estado, aircraft]
+                lista_ocupacion.append(info_gate)
+
+                a = a + 1
+
+            j = j + 1
+
+        i = i + 1
+
+    return lista_ocupacion
+
+#======================================== COMPROBAR AEROLÍNEA A TERMINAL CORRECTA ======================================
+def is_airline_in_terminal(terminal, name):
+#FUNCIÓN: comprueba si una aerolínea está operando en la terminal correcto ("asignada")
+    if name == "":
+        return False
+    if len(terminal.airline_icao_codes) == 0:
+        return False
+
+    i = 0
+    while i < len(terminal.airline_icao_codes):
+        if terminal.airline_icao_codes[i] == name:
+            return True
+        i = i + 1
+    return False
